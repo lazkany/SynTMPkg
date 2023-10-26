d10efb1254b9345478e5d017066a8b264db30d56
Yehia Abd Alrahman <lazkany@users.noreply.github.com>
2023-10-26 21:14:29 +0200

    statistics

--- generated/Priorty3Arb.java
+++ generated/Priorty3Arb.java
@@ -21,21 +21,21 @@ import com.syntm.lts.Spec;
 import com.syntm.lts.State;
 import com.syntm.lts.TS;
 
-public class Priorty2Arb {
+public class Priorty3Arb {
     private TS mainTS;
       public static void main(final String[] args)
               throws IOException, InterruptedException, ExecutionException, TimeoutException {
-          Priorty2Arb runEngine = new Priorty2Arb();
+          Priorty3Arb runEngine = new Priorty3Arb();
           Spec spec = new Spec();
-          spec.setcCode("r0,rm");
-          spec.setoCode("g0,gm");
-          Set<String> chan = new HashSet<>(Arrays.asList("r0,rm".split(",")));
-          Set<String> out = new HashSet<>(Arrays.asList("g0,gm".split(",")));
+          spec.setcCode("rm,r0,r1");
+          spec.setoCode("gm,g0,g1");
+          Set<String> chan = new HashSet<>(Arrays.asList("rm,r0,r1".split(",")));
+          Set<String> out = new HashSet<>(Arrays.asList("gm,g0,g1".split(",")));
           spec.getsInterface().setChannels(chan);
           spec.getsInterface().setOutput(out);
-          spec.agentBuilder("A2:rm:gm;A1:r0:g0;");
+          spec.agentBuilder("A1:r1:g1;A0:r0:g0;Am:rm:gm;");
           spec.assumptionBuilder("G F !rm".split(","));
-          spec.guaranteeBuilder("G (!gm | !g0),G (rm -> X (!g0 U gm)),G (r0 -> F g0),G ((g0 & X (!r0 & !g0)) -> X (r0 R !g0)),G ((gm & X (!rm & !gm)) -> X (rm R !gm)),G ((g0 & G !r0) -> (F !g0)),G ((gm & G !rm) -> (F !gm))".split(","));
+          spec.guaranteeBuilder("G ((!gm & !g0) | ((!gm | !g0) & !g1)),G (rm -> X ((!g0 & !g1) U gm)),G (r0 -> F g0),G (r1 -> F g1),G ((g0 & X (!r0 & !g0)) -> X (r0 R !g0)),G ((g1 & X (!r1 & !g1)) -> X (r1 R !g1)),G ((gm & X (!rm & !gm)) -> X (rm R !gm))".split(","));
 
     String command = spec.toString();
     System.out.println(command);
